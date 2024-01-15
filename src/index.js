d131b4f8bd80f4e8a28d030eac41736d7b5a20c2
Alen Kelemen <alen@edc.hr>
2024-01-15 13:53:33 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import './index.css'
-import { api } from './controls/api'
 import locale from './lang/hr.json'
 
-const wmsUrl = locale.rasterDOFPath
-const params = new URLSearchParams({
+const param = {
     SERVICE: 'WMS',
     VERSION: '1.3.0',
     REQUEST: 'GetMap',
@@ -12,11 +10,18 @@ const params = new URLSearchParams({
     layers: 'cp:CP.CadastralParcel',
     WIDTH: 256,
     HEIGHT: 256,
+    STYLES: "",
     CRS: 'EPSG:3857',
-    BBOX: '1803913.8675301597,5749287.519497817,1805136.8599827224,5750510.51195038'
-}) 
-const imageUrl = `${wmsUrl}?${params}`
-console.log(imageUrl)
+    BBOX: '1807888.5930009894,5748370.275158394,1808194.34111413,5748676.023271535'
+}
+
+const params = new URLSearchParams(param)
 const imageElement = document.createElement('img')
-imageElement.src = imageUrl
+imageElement.src = `${locale.rasterCPPath}?${params}`
 document.body.appendChild(imageElement)
+
+param.layers = 'DOF_TOPONIMI'
+const params1 = new URLSearchParams(param)
+const imageElement1 = document.createElement('img')
+imageElement1.src = `${locale.rasterDOFPath}?${params1}`
+document.body.appendChild(imageElement1)
